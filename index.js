3d83d25d9c4f0133b7892c04aaeae88aecf00a4d
CrosscutX <crosscutcode@gmail.com>
2023-02-16 02:23:20 -0500

    Guest and home are both functional

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-scoreNumbersHome = document.getElementById("score-numbers-home")
-scoreNumbersGuest = document.getElementById("score-numbers-guest")
-newGame = document.getElementById("new-game")
+scoreNumbersHome = document.getElementById("score-numbers-home");
+scoreNumbersGuest = document.getElementById("score-numbers-guest");
+newGame = document.getElementById("new-game");
 
-let countHome = 0
-let countGuest = 0
+let countHome = 0;
+let countGuest = 0;
 
-function homeIncrementOne() {
-    countHome += 1
-    scoreNumbersHome.textContent = countHome
-}
+function increment(team, amount) {
+  if (team === "home") {
+    countHome += amount;
+    scoreNumbersHome.textContent = countHome;
+  }
 
-function homeIncrementTwo() {
-    countHome += 2
-    scoreNumbersHome.textContent = countHome
-}
-
-function homeIncrementThree() {
-    countHome += 3
-    scoreNumbersHome.textContent = countHome
-}
-
-function guestIncrementOne() {
-    countGuest += 1
-    scoreNumbersGuest.textContent = countGuest
-}
-
-function guestIncrementTwo() {
-    countGuest += 2
-    scoreNumbersGuest.textContent = countGuest
-}
-
-function guestIncrementThree() {
-    countGuest += 3
-    scoreNumbersGuest.textContent = countGuest
+  if (team === "guest") {
+    countGuest += amount;
+    scoreNumbersGuest.textContent = countGuest;
+  }
 }
 
 function resetGame() {
-    countHome = 0
-    countGuest = 0
-    scoreNumbersGuest.textContent = 0
-    scoreNumbersHome.textContent = 0
-
+  countHome = 0;
+  countGuest = 0;
+  scoreNumbersGuest.textContent = 0;
+  scoreNumbersHome.textContent = 0;
 }
